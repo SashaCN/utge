7d996c8927a3875f967c5b895e32552c39db1bc2
IgorDikal <igrdkl@icloud.com>
2022-05-16 15:55:48 +0300

    add seeder-factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->state(),
+            'description' => $this->faker->text($maxNbChars = 200),
+            'article' => $this->faker->text($maxNbChars = 10),
+            'shipable' => $this->faker->boolean(),
+            'available' => $this->faker->boolean(),
+            'max_order' => $this->faker->randomDigit(),
+            'list_position' => $this->faker->randomDigit(),
+        ];
+    }
+}
