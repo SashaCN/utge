0a5a6824d9e06de62b741d96f6cc61ef006334ba
IgorDikal <igrdkl@icloud.com>
2022-05-18 16:03:29 +0300

    working image saver

--- app/helpers/imageSaver.php
+++ app/helpers/imageSaver.php
@@ -9,12 +9,13 @@
         public function upload():Image
         {
             $file = request()->file('image');
-            $uploadFolder = 'storage/folder';
+            $uploadFolder = 'public';
             $fileName = $file->getClientOriginalName();
+            $dblink = 'storage';
 
             Storage::putFileAs($uploadFolder, $file, $fileName);
 
-            return new Image(['url' => $uploadFolder.'/'.$fileName]);
+            return new Image(['url' => $dblink.'/'.$fileName]);
         }
     }
 ?>
