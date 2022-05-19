d5f800a1b6d54c186f7a47b235a01603b50798c1
SashaCN <sashacn228@gmail.com>
2022-05-19 13:50:06 +0300

    show products in categories

--- resources/views/admin/category/show.blade.php
+++ resources/views/admin/category/show.blade.php
@@ -8,7 +8,23 @@
         </tr>
         <tr>
             <td>{{$category->title}}</td>
-            <td></td>
+            <td>
+                <table>
+                    <tr>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>Article</th>
+                    </tr>
+                    @foreach ($products as $product)
+                    <tr>
+                        <td>{{$product->title}}</td>
+                        <td>{{$product->description}}</td>
+                        <td>{{$product->article}}</td>
+                        <td><a href="{{ route('product.show', $product->id) }}">show</a></td>
+                        <td><img src="{{ asset($product->image->url) }}" alt="{{ $product->image->alt }}"></td>
+                    </tr>
+                    @endforeach
+            </td>
         </tr>
     </table>
 @endsection
