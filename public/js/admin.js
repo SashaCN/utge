5eeff460e99393a7d80d67ede9c809ec45d9b801
SashaCN <sashacn228@gmail.com>
2022-06-17 12:49:58 +0300

    drop-menu in aside

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,29 +1,41 @@
 // add menu toggle
 
 
-let add_button = document.querySelector(".add"),
-add_menu = document.querySelector(".add-menu");
+let drop_btn = document.querySelectorAll(".drop-btn"),
+    drop_list = document.querySelectorAll(".drop-list");
+
+for (let i = 0; i < drop_btn.length; i++) {
+    drop_btn[i].setAttribute('data-number', i);
+    drop_list[i].setAttribute('data-number', i);
+}
 
 document.onclick = (e) => {
-  changeLang(e);
-  if (e.target == add_button) {
-    add_button.classList.toggle('focus');
-    add_menu.classList.toggle('hidden');
-  } else {
-    add_button.classList.remove('focus');
-    add_menu.classList.add('hidden');
-  }
+    changeLang(e);
+    drop_btn.forEach(elem => {
+        if (e.target == elem || elem.contains(e.target)) {
+            e.preventDefault();
+            elem.classList.toggle('focus');
+            drop_list[elem.getAttribute('data-number')].classList.toggle('hidden');
+        } else {
+            elem.classList.remove('focus');
+            drop_list[elem.getAttribute('data-number')].classList.add('hidden');
+        }
+    });
 }
 
 // chosen list item
 
-let list = document.querySelectorAll(".aside-menu li:nth-child(1n+2)"),
-    selectedItem;
+let list = document.querySelectorAll(".aside-menu li a"),
+selectedItem;
 
 list.forEach((elem) => {
 
-  if (elem.querySelector("a").href == location.href) {
-    elem.classList.add('selected-item');
+  if (elem.href == location.href) {
+      elem.classList.add('selected-item');
+      if (elem.closest('.drop-list') != null) {
+        elem.closest('.drop-list').closest('li').querySelector('.drop-btn').classList.add('focus');
+        elem.closest('.drop-list').classList.remove('hidden');
+    }
   }
 });
 
--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
     <title>@lang('admin.utge_admin')</title>
-    
+
 </head>
 <body>
     <header>
@@ -25,9 +25,9 @@
 
                 </li>
                 <li class="li-add">
-                    <a href="#" class="add">@lang('admin.add')</a>
+                    <a href="#" class="add drop-btn">@lang('admin.add')</a>
 
-                    <div class="add-menu hidden">
+                    <div class="add-menu drop-list hidden">
                         <ul>
                             <li><a href="{{ route('product.create') }}">@lang('admin.product')</a></li>
                             <li><a href="{{ route('subCategory.create') }}">@lang('admin.sub_category_add')</a></li>
@@ -49,15 +49,26 @@
         </div>
     </header>
     <aside>
+        <p class="logo">
+            <a href="{{ route('index') }}" class="flex">
+                <img src="{{ asset('img/logo.png') }}" alt="Hashtag logo">
+            </a>
+        </p>
         <nav>
             <ul class="aside-menu">
-                <li class="logo">
-                    <a href="{{ route('index') }}" class="flex">
-                        <img src="{{ asset('img/logo.png') }}" alt="Hashtag logo">
-                    </a>
-                </li>
                 <li><a href="{{ route('product.index') }}"><span class="link-text">@lang('admin.products')</span></a></li>
-                <li><a href="{{ route('category.index') }}"><span class="link-text">@lang('admin.categories')</span></a>    </li>
+                <li>
+                    <a href="#" class="drop-`"><span class="link-text">@lang('admin.categories')</span></a>
+
+                    <div class="drop-list hidden">
+                        <ul>
+                            <li><a href="{{ route('category.index') }}">@lang('admin.categories')</a></li>
+                            <li><a href="{{ route('subCategory.create') }}">2</a></li>
+                            <li><a href="{{ route('category.create') }}">3</a></li>
+                            <li><a href="{{ route('productType.create') }}">4</a></li>
+                        </ul>
+                    </div>
+                </li>
                 <li><a href="#"><span class="link-text">@lang('admin.orders')</span></a></li>
                 <li><a href="{{ route('news.index') }}"><span class="link-text">@lang('admin.news')</span></a></li>
                 <li><a href="{{ route('childPage.index') }}"><span class="link-text">@lang('admin.modules')</span></a></li>
