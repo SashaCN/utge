ac99b586b617f600cb7959dd577483cbe879828f
IgorDikal <igrdkl@icloud.com>
2022-07-04 19:10:38 +0300

    add image/desc services, add view site services

--- app/Models/ServicesType.php
+++ app/Models/ServicesType.php
@@ -5,13 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Dyrynda\Database\Support\CascadeSoftDeletes;
-use Illuminate\Support\Facades\DB;
 
-class ServicesType extends Model
+class ServicesType extends Model implements HasMedia
 {
     use HasFactory;
     use SoftDeletes, CascadeSoftDeletes;
+    use InteractsWithMedia;
 
     protected $cascadeDeletes = ['categories'];
     protected $dates = ['deleted_at'];
