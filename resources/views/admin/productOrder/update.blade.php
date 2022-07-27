a7b00abbec6964b713a483501de42f8509d8d173
igrdkl <igrdkl@icloud.com>
2022-07-27 12:42:32 +0300

    add midlle process

--- resources/views/admin/productOrder/update.blade.php
+++ resources/views/admin/productOrder/update.blade.php
@@ -18,68 +18,71 @@ $locale = app()->getLocale();
             </tr>
             <tr>
                 <td>@lang('utge.firstname'):</td>
-                <td>{{$customers->firstname}}</td>
+                <td class="table-desc-contact">{{$customers->firstname}}</td>
             </tr>
             <tr>
                 <td>@lang('utge.lastname'): </td>
-                <td>{{$customers->lastname}}</td>
+                <td class="table-desc-contact">{{$customers->lastname}}</td>
             </tr>
             <tr>
-                <td>@lang('admin.phone'): </td>
-                <td> {{$customers->phone}}</td>
+                <td>@lang('utge.number-phone'): </td>
+                <td class="table-desc-contact"> {{$customers->phone}}</td>
             </tr>
             <tr>
-                <td>@lang('admin.city')</td>
-                <td>{{$customers->city}}</td>
+                <td>@lang('admin.city'):</td>
+                <td class="table-desc-contact">{{$customers->city}}</td>
             </tr>
             <tr>
-                <td>@lang('admin.adress')</td>
-                <td>{{$customers->adress_delivery}}</td>
+                <td>@lang('admin.adress'):</td>
+                <td class="table-desc-contact">{{$customers->adress_delivery}}</td>
             </tr>
             <tr>
-                <td>@lang('utge.delivery')</td>
+                <td>@lang('utge.delivery'):</td>
                 @switch($customers->delivery_type)
                     @case('nova')
-                        <td>@lang('admin.nova-poshta')</td>
+                        <td class="table-desc-contact">@lang('admin.nova-poshta')</td>
                         @break
                     @case('ind')
-                        <td>@lang('admin.ind-delivery')</td>
+                        <td class="table-desc-contact">@lang('admin.ind-delivery')</td>
                         @break
                     @case('adres')
-                        <td>@lang('admin.adres-delivery')</td>
+                        <td class="table-desc-contact">@lang('admin.adres-delivery')</td>
                         @break
                     @case('int')
-                        <td>@lang('admin.intime')</td>
+                        <td class="table-desc-contact">@lang('admin.intime')</td>
                         @break
                     @case('avl')
-                        <td>@lang('admin.avtolux')</td>
+                        <td class="table-desc-contact">@lang('admin.avtolux')</td>
+                        @break
+                    @case('ukr')
+                        <td class="table-desc-contact">@lang('admin.ukr-poshta')</td>
                         @break
                     @default
                 @endswitch
             </tr>
 
             <tr>
-                <td>@lang('admin.payment')</td>
+                <td>@lang('admin.payment'):</td>
                 @switch($customers->payment_type)
                     @case('cash')
-                        <td>@lang('admin.cash')</td>
+                        <td class="table-desc-contact">@lang('admin.cash')</td>
                         @break
                     @case('privat')
-                        <td>@lang('admin.privat')</td>
+                        <td class="table-desc-contact">@lang('admin.privat')</td>
                         @break
                     @case('cart')
-                        <td>@lang('admin.cart')</td>
+                        <td class="table-desc-contact">@lang('admin.cart')</td>
                         @break
                     @default
                 @endswitch
             </tr>
 
         </table>
-        <form id="services-form" class="services-order-form" action="{{ route('productsOrder.update', $customers->id) }}" method="POST">
+        <form id="order-product-form" class="order-product-form" action="{{ route('productsOrder.update', $customers->id) }}" method="POST">
             @csrf
             @method('PUT')
 
-
+            <p>@lang('admin.status')</p>
             <div class="order-select-wrap">
                 <select name="status">
 
@@ -100,15 +103,22 @@ $locale = app()->getLocale();
 
             </div>
 
-            <button for="services-form" type="submit">@lang('admin.change')</button>
+            <button for="order-product-form" type="submit">@lang('admin.change')</button>
 
 
         </form>
     </div>
-    <div>
+    <div class="order-list-wrap">
         <table class="order-list-table">
             <tr>
                 <td class="table-desc">@lang('admin.order-product-list')</td>
+            </tr>
+            <tr>
+                {{-- <td>2</td> --}}
+                <td class="table-desc-item">@lang('admin.product')</td>
+                <td class="table-desc-item">@lang('utge.quatify')</td>
+                <td class="table-desc-item">@lang('admin.price-of-unit')</td>
+                <td class="table-desc-item">@lang('admin.gen-price-unit')</td>
             </tr>
             @foreach ($orders as $order)
                 @foreach ($products as $product)
@@ -118,7 +128,7 @@ $locale = app()->getLocale();
                     @endphp
                     @if ($order->product_id == $product->id)
                     <tr>
-                        <td><img class="order-product-img" src="{{ $product->getFirstMediaUrl('images') }}" alt="{{ $title->$locale }}"></label></td>
+                        {{-- <td><img class="order-product-img" src="{{ $product->getFirstMediaUrl('images') }}" alt="{{ $title->$locale }}"></label></td> --}}
                         <td>{{ $title->$locale }}</td>
                         <td>{{ $order->quantity }}</td>
                         <td>{{ $order->price }}</td>
@@ -131,8 +141,8 @@ $locale = app()->getLocale();
                 @endforeach
             @endforeach
             <tr>
-                <td>@lang('admin.total_price')</td>
-                <td>{{array_sum($gen_price)}}</td>
+                <td class="table-desc-total-price">@lang('admin.total_price')</td>
+                <td class="table-desc-total-price">{{array_sum($gen_price)}}</td>
             </tr>
         </table>
     </div>
