1229ea7bbffd9ab75db79d59f40e673085c3d400
SashaCN <sashacn228@gmail.com>
2022-07-27 19:42:14 +0300

    some fixs

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -10,6 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="shortcut icon" href="{{ asset('img/logo.png') }}" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @foreach ($seos as $seo)
         @php
