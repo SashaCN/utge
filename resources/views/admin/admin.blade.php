dd9d5df3d04ec5a82e2e62a5ef13d74b0756c6e1
Alexandr <sashacn228@gmail.com>
2022-10-11 14:06:18 +0300

    fix trach count

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -188,7 +188,7 @@
 
                 <li>
                     @if (!empty(($count = count($trashProduct))) || !empty(($count = count($trashService))))
-                        <a href="#" class="drop-btn trash-btn "><span class="link-text aside-menu-item">@lang('admin.trash')<span class="trash-circle-counter"><span>{{ count($trashProduct) + count($trashService) }}</span></span></a>
+                        <a href="#" class="drop-btn trash-btn "><span class="link-text aside-menu-item">@lang('admin.trash')<span class="trash-circle-counter"><span>{{ $trashProduct->total() + $trashService->total() }}</span></span></a>
                     @else
                         <a href="#" class="drop-btn trash-btn"><span class="link-text">@lang('admin.trash')</span></a>
                     @endif
@@ -197,17 +197,16 @@
                     <ul>
                         @if (!empty(($count = count($trashProduct))))
                             <li>
-                                <a href="{{ route('trashBox.index') }}" class="trash-btn"><span class="aside-menu-item link-text">@lang('admin.trash_box')<span class="trash-circle-counter"><span>{{ $count }}</span></span></a>
+                                <a href="{{ route('trashBox.index') }}" class="trash-btn"><span class="aside-menu-item link-text">@lang('admin.trash_box')<span class="trash-circle-counter"><span>{{ $trashProduct->total() }}</span></span></a>
                             </li>
                         @else
                             <li>
                                 <a href="{{ route('trashBox.index') }}" class="trash-btn"><span class="link-text">@lang('admin.trash_box')</span></a>
                             </li>
                         @endif
-        
                         @if (!empty(($count = count($trashService))))
                             <li>
-                                <a href="{{ route('service_trash_box') }}" class="trash-btn"><span class="link-text aside-menu-item">@lang('admin.service_trash_box')<span class="trash-circle-counter"><span>{{ $count }}</span></span></a>
+                                <a href="{{ route('service_trash_box') }}" class="trash-btn"><span class="link-text aside-menu-item">@lang('admin.service_trash_box')<span class="trash-circle-counter"><span>{{ $trashService->total() }}</span></span></a>
                             </li>
                         @else
                             <li>
@@ -216,7 +215,7 @@
                         @endif
                     </ul>
                 </div>
-                
+
             </ul>
         </nav>
         <p class="copy">
