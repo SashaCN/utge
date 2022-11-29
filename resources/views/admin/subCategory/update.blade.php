aa6fec60e34d1e5757ec34a413798cd3b5f1c1de
fisvif <shutkovadim@mail.ru>
2022-11-29 20:20:22 +0200

    try to find error with sub category of product

--- resources/views/admin/subCategory/update.blade.php
+++ resources/views/admin/subCategory/update.blade.php
@@ -7,14 +7,13 @@ $locale = app()->getLocale();
 @endphp
 
 @php
-$title = $subCategory->localization[0];
+  $title = $subCategory->localization[0];
 @endphp
 
 <div class="error">
     @if ($errors->any())
             @foreach ($errors->all() as $error)
                     <div class="error-item"><img class="error-icon" src="{{ asset('img/error.svg') }}" alt="error"><p class="error-desc">{{ $error }}</p></div>
-
             @endforeach
     @endif
 </div>
@@ -37,7 +36,17 @@ $title = $subCategory->localization[0];
     @method('PUT')
 
     @php
-        $title = $subCategory->localization[1];
+
+        
+        
+        if (isset($subCategory->localization[1]))
+        {
+            $title = $subCategory->localization[1];
+        }
+        else {
+            $title = "something went wrong with title";
+        }
+
         if (isset($subCategory->localization[0]))
         {
             $description = $subCategory->localization[0];
@@ -94,7 +103,7 @@ $title = $subCategory->localization[0];
                   <a class='btn' id="converToCode1" data-role='switchEditor' href='#' title='Перейти в редактор коду'>&lt;code&gt;</a>
                 </div>
               </div>
-              <div id='editor1' style='' contenteditable>@if (isset($subCategory->localization[1]) && $description->uk != 'utge undefined description') {!! $description->uk !!}@endif</div>
+              <div id='editor1' style='' contenteditable>@if (isset($subCategory->localization[0]) && $description->uk != 'utge undefined description') {!! $description->uk !!}@endif</div>
               <textarea id="desc_uk" name="sub_description_uk"></textarea>
               <label class="label" for="desc_uk">@lang('admin.add_uk_desc')</label>
             </div>
@@ -139,7 +148,7 @@ $title = $subCategory->localization[0];
                   <a class='btn' id="converToCode2" data-role='switchEditor' href='#' title='Перейти в редактор коду'>&lt;code&gt;</a>
                 </div>
               </div>
-              <div id='editor2' style='' contenteditable>@if (isset($subCategory->localization[1]) && $description->ru != 'utge undefined description') {!! $description->ru !!}@endif</div>
+              <div id='editor2' style='' contenteditable>@if (isset($subCategory->localization[0]) && $description->ru != 'utge undefined description') {!! $description->ru !!}@endif</div>
               <textarea id="desc_ru" name="sub_description_ru"></textarea>
               <label class="label" for="desc_ru">@lang('admin.add_ru_desc')</label>
             </div>
